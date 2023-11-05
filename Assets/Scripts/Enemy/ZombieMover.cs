fc83bd83ec444e1339f58b1557f38550a1bde5a7
Alex Trusov <84204007+SqueekDev@users.noreply.github.com>
2023-11-05 06:19:33 +0300

    Upgraded for build

--- Assets/Scripts/Enemy/ZombieMover.cs
+++ Assets/Scripts/Enemy/ZombieMover.cs
@@ -2,8 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
-using static UnityEngine.GraphicsBuffer;
 
 public class ZombieMover : Mover
 {
