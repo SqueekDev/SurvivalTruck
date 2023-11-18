75e0fe554cfce29d76151470cf9455cbc923967c
Igoryochek <83440261+Igoryochek@users.noreply.github.com>
2023-11-18 23:18:26 +1000

    ShopAddingChest

--- Assets/Scripts/Player/PlayerShooter.cs
+++ Assets/Scripts/Player/PlayerShooter.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class PlayerShooter : Shooter
 {
     [SerializeField] private UpgradesPanel _upgradesPanel;
+    [SerializeField] private Shop _shop;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent(out Shop shop) && IsShooting == false)
         {
-            _upgradesPanel.gameObject.SetActive(true);
+            StartCoroutine(CoverOpening());
         }
+    }
+
+    private IEnumerator CoverOpening()
+    {
+        _shop.Open();
+        yield return new WaitForSeconds(1);
+        _upgradesPanel.gameObject.SetActive(true);
+
     }
 }
--- Assets/Scripts/Shop/Shop.cs
+++ Assets/Scripts/Shop/Shop.cs
@@ -4,4 +4,14 @@ using UnityEngine;
 
 public class Shop : MonoBehaviour
 {
+    [SerializeField] private Animator _coverOpeningAnimator;
+
+    public void Open()
+    {
+        _coverOpeningAnimator.SetTrigger("Open");
+    }
+    public void Close()
+    {
+        _coverOpeningAnimator.SetTrigger("Close");
+    }
 }
