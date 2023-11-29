b290b13226928d0034a51a75f5c542527a97531b
Alex Trusov <84204007+SqueekDev@users.noreply.github.com>
2023-11-29 04:42:09 +0300

    Edit level platforms, lightning settings and skybox

--- Assets/Scripts/Level/EnviromentElements.cs
+++ Assets/Scripts/Level/EnviromentElements.cs
@@ -7,6 +7,8 @@ public class EnviromentElements : MonoBehaviour
     [SerializeField] private List<GameObject> _templates;
 
     private int _maxRotation = 360;
+    private float _minScale = 0.7f;
+    private float _maxScale = 1f;
 
     private void Start()
     {
@@ -14,5 +16,13 @@ public class EnviromentElements : MonoBehaviour
         int yRotarion = Random.Range(0, _maxRotation);
         Quaternion rotation = Quaternion.Euler(0, yRotarion, 0);
         Instantiate(_templates[currentTemplateNumber], transform.position, rotation, transform);
+        float scaleModifier = Random.Range(_minScale, _maxScale);
+        transform.localScale *= scaleModifier;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out EnviromentElements element))
+            Destroy(gameObject);
     }
 }
